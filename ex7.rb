9efb4daa0271d0651bb54de18364306ef8cc40fd
ottaviana <ottavianadavenia@gmail.com>
2017-01-12 11:42:54 +0100

    Finish execise 7

---
+++ ex7.rb
@@ -0,0 +1,29 @@
+# it shows the sentence
+puts "Mary had a little lamb."
+#snow =  "carbon"
+#since there are the '' it shows snow (in this case is not a variable)
+puts "Its fleece was white as #{'snow'}."
+puts "And everywhere that Mary went."
+# it will print 10 times the dot
+puts "." * 10
+
+#variables
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+#only print change tha name of the user director adding (in this case summing) the letter i want to
+print end1 + end2 + end3 + end4 + end5 + end6
+# together with print put will merge producing the word CheeseBurger
+puts end7 + end8 + end9 + end10 + end11 + end12
+
+# puts "CheeseBurger" i would have done it like this
