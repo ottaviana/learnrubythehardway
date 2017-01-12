101126c3d5cd9632b7a1523a4aadc02ab3f226cf
ottaviana <ottavianadavenia@gmail.com>
2017-01-12 11:42:26 +0100

    Finish execise 6

---
+++ ex6.rb
@@ -0,0 +1,27 @@
+# this is a variable
+types_of_people = 10
+# this is a string containing the a variable 
+x = "There are #{types_of_people} types of people"
+binary = "binary"
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}."
+
+puts x
+puts y
+
+puts "I said: #{x}."
+#the output will be a string in quotation mark
+puts "I also said: '#{y}'."
+
+#this is a variable
+hilarious = false
+# this is a string containing the a variable
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+puts joke_evaluation
+
+
+w = "This is the left side of..."
+e = "a string with a right side."
+# this will merge the two sentence
+puts w + e
