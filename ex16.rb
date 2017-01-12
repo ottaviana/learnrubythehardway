c4afe52d9eb1a010b6af973c6e4475900eb5e7f5
ottaviana <ottavianadavenia@gmail.com>
2017-01-12 11:41:43 +0100

    Finish execise 16

---
+++ ex16.rb
@@ -0,0 +1,12 @@
+filename = ARGV.first
+
+puts "We're going to erase #{filename}"
+puts "If you don't want that, hit CTRL-C (^C)."
+puts "If you do want that, hit RETURN."
+
+$stdin.gets
+
+puts "Opening the file..."
+target = open(filename)
+
+print target.read
