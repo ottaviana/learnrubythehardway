28689c6b0a0ed708ec315ff9701bdd212f05b433
ottaviana <ottavianadavenia@gmail.com>
2017-01-12 11:40:09 +0100

    Finish execise 5

---
+++ ex5.rb
@@ -0,0 +1,17 @@
+name = 'Zed A. Shaw'
+age = 18 + 20
+cm = 2.54 #ho cambiato lbs in cm
+height =  80 * cm # 80 si riferisce a lbs
+weight = 30 +17 *2 #lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall"
+puts "He's #{weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes}eyes and #{hair}"
+puts "His teeth are usually #{teeth} depending on the cofee."
+
+puts "If I add #{age}, #{height}, #{weight}, I get #{age + height + weight}."
