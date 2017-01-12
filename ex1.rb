90b34bd889361deeb648fa7d795d7f00fca4a180
ottaviana <ottavianadavenia@gmail.com>
2017-01-12 11:14:42 +0100

    Finish execise 1

---
+++ ex1.rb
@@ -0,0 +1,11 @@
+puts "Hello World"
+puts "ciao"
+puts "glasses"
+puts "kdsjkfcjsdlfjc"
+puts "hsudfhsidhf"
+puts "dkjfnskdjfn"
+puts 'I "said" do not touch this.'
+puts "Yay! Printing."
+puts "two plus two equals #{2 + 2}"
+# puts 'It\'s a lovely day outside'
+# puts "ciao ciao"
